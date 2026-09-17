7a3b4f323f43939cbf690f5e95661a366fc229f9
rax0h <youvegotfalcon@gmail.com>
2026-09-16 21:17:07 -0500

    test: calibrate ordinary essence affordability

--- simulation/tests/test_magic_access.py
+++ simulation/tests/test_magic_access.py
@@ -4,8 +4,8 @@ class R:
  def __init__(self,kind,rarity):self.kind=kind;self.rarity=rarity
 
 def test_common_and_uncommon_essences_are_ordinary_affordable_goods():
- assert _price(R('essence','Common'))==3.
- assert _price(R('essence','Uncommon'))==6.
+ assert _price(R('essence','Common'))==1.
+ assert _price(R('essence','Uncommon'))==2.
  assert _price(R('essence','Rare'))>_price(R('essence','Uncommon'))
  assert _price(R('essence','Legendary'))>=20*_price(R('essence','Uncommon'))
 
